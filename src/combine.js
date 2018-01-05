3eefed03969ddfba85fc3f4e29057dc1cc2c7ebe
Kaushik Nagaraj <kaushikstan@gmail.com>
2018-01-04 21:57:37 -0500

    created new index

---
+++ src/combine.js
@@ -0,0 +1,11 @@
+import TextMessage from './TextMessage.js';
+import WhatsappMessage from './WhatsappMessage.js';
+import FacebookMessage from './FacebookMessage.js';
+import FBMessengerShareLink from './FBMessengerShareLink.js';
+
+export {
+  TextMessage,
+  WhatsappMessage,
+  FacebookMessage,
+  FBMessengerShareLink
+}
