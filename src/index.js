88491b2310d31f3b8d6b99c54456db1089940a72
Kaushik Nagaraj <kaushikstan@gmail.com>
2018-01-04 21:46:37 -0500

    import error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import _TextMessage from './TextMessage.js';
-import _WhatsappMessage from './WhatsappMessage.js';
-import _FacebookMessage from './FacebookMessage.js';
-import _FBMessengerShareLink from './FBMessengerShareLink.js';
+import TextMessage from './TextMessage.js';
+import WhatsappMessage from './WhatsappMessage.js';
+import FacebookMessage from './FacebookMessage.js';
+import FBMessengerShareLink from './FBMessengerShareLink.js';
 
 export {
-  _TextMessage as TextMessage,
-  _WhatsappMessage as WhatsappMessage,
-  _FacebookMessage as FacebookMessage,
-  _FBMessengerShareLink as FBMessengerShareLink
+  TextMessage,
+  WhatsappMessage,
+  FacebookMessage,
+  FBMessengerShareLink
 }
