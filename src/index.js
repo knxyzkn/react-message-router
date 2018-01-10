04f1ed94377de8c618e7299728d79e0780672986
Kaushik Nagaraj <kaushikstan@gmail.com>
2018-01-09 19:57:39 -0500

    configured export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { TextMessage, WhatsappMessage, FacebookMessage, FBMessengerShareLink } from './combine.js';
+import TextMessage from './TextMessage.js';
+import WhatsappMessage from './WhatsappMessage.js';
+import FacebookMessage from './FacebookMessage.js';
+import FBMessengerShareLink from './FBMessengerShareLink.js';
 
-export { TextMessage, WhatsappMessage, FacebookMessage, FBMessengerShareLink };
+export {
+  TextMessage,
+  WhatsappMessage,
+  FacebookMessage,
+  FBMessengerShareLink
+}
