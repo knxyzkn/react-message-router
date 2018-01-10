6ad73bbd8073ba78b0470c6739a84802707c78b1
Kaushik Nagaraj <kaushikstan@gmail.com>
2018-01-09 21:08:31 -0500

    new build

--- build/index.js
+++ build/index.js
@@ -2,64 +2,428 @@ module.exports =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-
+/******/ 		module.l = true;
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	import TextMessage from './TextMessage.js';
-	export default TextMessage;
-	// import WhatsappMessage from './WhatsappMessage.js';
-	// import FacebookMessage from './FacebookMessage.js';
-	// import FBMessengerShareLink from './FBMessengerShareLink.js';
-	//
-	// const ReactMessageRouter = {
-	//   TextMessage,
-	//   WhatsappMessage,
-	//   FacebookMessage,
-	//   FBMessengerShareLink
-	// }
-	//
-	// export default ReactMessageRouter;
+"use strict";
+/*!
+ * encodeurl
+ * Copyright(c) 2016 Douglas Christopher Wilson
+ * MIT Licensed
+ */
 
 
-/***/ }
+
+/**
+ * Module exports.
+ * @public
+ */
+
+module.exports = encodeUrl
+
+/**
+ * RegExp to match non-URL code points, *after* encoding (i.e. not including "%")
+ * and including invalid escape sequences.
+ * @private
+ */
+
+var ENCODE_CHARS_REGEXP = /(?:[^\x21\x25\x26-\x3B\x3D\x3F-\x5B\x5D\x5F\x61-\x7A\x7E]|%(?:[^0-9A-Fa-f]|[0-9A-Fa-f][^0-9A-Fa-f]))+/g
+
+/**
+ * RegExp to match unmatched surrogate pair.
+ * @private
+ */
+
+var UNMATCHED_SURROGATE_PAIR_REGEXP = /(^|[^\uD800-\uDBFF])[\uDC00-\uDFFF]|[\uD800-\uDBFF]([^\uDC00-\uDFFF]|$)/g
+
+/**
+ * String to replace unmatched surrogate pair with.
+ * @private
+ */
+
+var UNMATCHED_SURROGATE_PAIR_REPLACE = '$1\uFFFD$2'
+
+/**
+ * Encode a URL to a percent-encoded form, excluding already-encoded sequences.
+ *
+ * This function will take an already-encoded URL and encode all the non-URL
+ * code points. This function will not encode the "%" character unless it is
+ * not part of a valid sequence (`%20` will be left as-is, but `%foo` will
+ * be encoded as `%25foo`).
+ *
+ * This encode is meant to be "safe" and does not throw errors. It will try as
+ * hard as it can to properly encode the given URL, including replacing any raw,
+ * unpaired surrogate pairs with the Unicode replacement character prior to
+ * encoding.
+ *
+ * @param {string} url
+ * @return {string}
+ * @public
+ */
+
+function encodeUrl (url) {
+  return String(url)
+    .replace(UNMATCHED_SURROGATE_PAIR_REGEXP, UNMATCHED_SURROGATE_PAIR_REPLACE)
+    .replace(ENCODE_CHARS_REGEXP, encodeURI)
+}
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _encodeurl = __webpack_require__(0);
+
+var _encodeurl2 = _interopRequireDefault(_encodeurl);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var FBMessengerShareLink = function (_React$Component) {
+  _inherits(FBMessengerShareLink, _React$Component);
+
+  function FBMessengerShareLink() {
+    _classCallCheck(this, FBMessengerShareLink);
+
+    return _possibleConstructorReturn(this, (FBMessengerShareLink.__proto__ || Object.getPrototypeOf(FBMessengerShareLink)).apply(this, arguments));
+  }
+
+  _createClass(FBMessengerShareLink, [{
+    key: 'render',
+    value: function render() {
+      var link = this.props.link ? (0, _encodeurl2.default)(this.props.link) : '';
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'a',
+          { href: 'fb-messenger://share/?link=' + link, style: this.props.style },
+          this.props.label
+        )
+      );
+    }
+  }]);
+
+  return FBMessengerShareLink;
+}(_react2.default.Component);
+
+exports.default = FBMessengerShareLink;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _encodeurl = __webpack_require__(0);
+
+var _encodeurl2 = _interopRequireDefault(_encodeurl);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var FacebookMessage = function (_React$Component) {
+  _inherits(FacebookMessage, _React$Component);
+
+  function FacebookMessage() {
+    _classCallCheck(this, FacebookMessage);
+
+    return _possibleConstructorReturn(this, (FacebookMessage.__proto__ || Object.getPrototypeOf(FacebookMessage)).apply(this, arguments));
+  }
+
+  _createClass(FacebookMessage, [{
+    key: 'render',
+    value: function render() {
+      var profileName = this.props.profileName ? this.props.profileName : '';
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'a',
+          { href: 'https://m.me/' + profileName, style: this.props.style },
+          this.props.label
+        )
+      );
+    }
+  }]);
+
+  return FacebookMessage;
+}(_react2.default.Component);
+
+exports.default = FacebookMessage;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _encodeurl = __webpack_require__(0);
+
+var _encodeurl2 = _interopRequireDefault(_encodeurl);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var TextMessage = function (_React$Component) {
+  _inherits(TextMessage, _React$Component);
+
+  function TextMessage() {
+    _classCallCheck(this, TextMessage);
+
+    return _possibleConstructorReturn(this, (TextMessage.__proto__ || Object.getPrototypeOf(TextMessage)).apply(this, arguments));
+  }
+
+  _createClass(TextMessage, [{
+    key: 'render',
+    value: function render() {
+      var body = this.props.textBody ? (0, _encodeurl2.default)(this.props.textBody) : '';
+      var number = this.props.number ? this.props.number : '';
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'a',
+          { href: 'sms:' + number + '&body=' + body, style: this.props.style },
+          this.props.label
+        )
+      );
+    }
+  }]);
+
+  return TextMessage;
+}(_react2.default.Component);
+
+exports.default = TextMessage;
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _encodeurl = __webpack_require__(0);
+
+var _encodeurl2 = _interopRequireDefault(_encodeurl);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var WhatsappMessage = function (_React$Component) {
+  _inherits(WhatsappMessage, _React$Component);
+
+  function WhatsappMessage() {
+    _classCallCheck(this, WhatsappMessage);
+
+    return _possibleConstructorReturn(this, (WhatsappMessage.__proto__ || Object.getPrototypeOf(WhatsappMessage)).apply(this, arguments));
+  }
+
+  _createClass(WhatsappMessage, [{
+    key: 'render',
+    value: function render() {
+      var body = this.props.textBody ? (0, _encodeurl2.default)(this.props.textBody) : '';
+      var number = this.props.number ? this.props.number : '';
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'a',
+          { href: 'https://api.whatsapp.com/send?phone=' + number + '&text=' + body, style: this.props.style },
+          this.props.label
+        )
+      );
+    }
+  }]);
+
+  return WhatsappMessage;
+}(_react2.default.Component);
+
+exports.default = WhatsappMessage;
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _TextMessage = __webpack_require__(4);
+
+Object.defineProperty(exports, 'TextMessage', {
+  enumerable: true,
+  get: function get() {
+    return _TextMessage.TextMessage;
+  }
+});
+
+var _WhatsappMessage = __webpack_require__(5);
+
+Object.defineProperty(exports, 'WhatsappMessage', {
+  enumerable: true,
+  get: function get() {
+    return _WhatsappMessage.WhatsappMessage;
+  }
+});
+
+var _FacebookMessage = __webpack_require__(3);
+
+Object.defineProperty(exports, 'FacebookMessage', {
+  enumerable: true,
+  get: function get() {
+    return _FacebookMessage.FacebookMessage;
+  }
+});
+
+var _FBMessengerShareLink = __webpack_require__(2);
+
+Object.defineProperty(exports, 'FBMessengerShareLink', {
+  enumerable: true,
+  get: function get() {
+    return _FBMessengerShareLink.FBMessengerShareLink;
+  }
+});
+
+/***/ })
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import TextMessage from './TextMessage.js';
-export default TextMessage;
+// import TextMessage from './TextMessage.js';
+// export {TextMessage as T};
+export {TextMessage} from './TextMessage.js';
+export {WhatsappMessage} from './WhatsappMessage.js';
+export {FacebookMessage} from './FacebookMessage.js';
+export {FBMessengerShareLink} from './FBMessengerShareLink.js';
 // import WhatsappMessage from './WhatsappMessage.js';
 // import FacebookMessage from './FacebookMessage.js';
 // import FBMessengerShareLink from './FBMessengerShareLink.js';
